dcdae8fde5c69f60191ddf32d1da354b30696e82
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2025-06-03 21:16:11 -0300

    class: Seção: Hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
+import { HeroSection } from "@/components/hero-section"
 
 export default function Home() {
   return (
-   <div className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}>
-    <div className="flex min-h-screen flex-col items-center justify-center p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-3xl font-bold">Início</h1>
-      <p className="text-lg text-muted-foreground">Welcome to the início page!</p>
-    </div>
-   </div>
+    <>
+    <article className="flex flex-col">
+      <HeroSection />
+    </article>
+  </>
   )
 }
